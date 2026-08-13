2d3e63657d3391fbffec33a7c36c1a645c10e3bb
Isaac Saunders <git@eartharoid.me>
2026-08-13 21:31:37 +0100

    refactor(api): don't fail silently

--- src/routes/api/admin/guilds/[guild]/data.js
+++ src/routes/api/admin/guilds/[guild]/data.js
@@ -1,17 +1,21 @@
 module.exports.get = fastify => ({
-	handler: async req => {
+	handler: async (req, res) => {
 		/** @type {import('client')} */
 		const client = req.routeOptions.config.client;
 		const id = req.params.guild;
 		const guild = client.guilds.cache.get(id) ?? {};
 		const { query } = req.query;
-		switch (query ?? '') {
+		switch (query) {
 		case 'channels.cache':
 			return guild.channels.cache;
 		case 'roles.cache':
 			return guild.roles.cache;
 		default:
-			return {};
+			return res.code(400).send({
+				error: 'Bad Request',
+				message: 'Invalid query parameter.',
+				statusCode: 400,
+			});;
 		}
 	},
 	onRequest: [fastify.authenticate, fastify.isAdmin],
